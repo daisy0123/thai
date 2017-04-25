fe126c1d89439f132ffd99610766e1b08f5ef47f
qiuyuxiu <20141002345qyx>
2017-04-25 10:03:40 +0800

    后台数据get请求修复

--- routes/index.js
+++ routes/index.js
@@ -16,12 +16,12 @@ router.get('/',function(req,res){
 
 router.post('/searchword',function(req,res,next){
     var word = req.body.word;
-    var path="/search/search？search_word"+word;
-    //var url="http://192.168.203.94:8000/search/search?";
+    //var path="/search/search？search_word"+word;
+    var url="http://192.168.203.94:8000/search/search?search_word="+word;
     // var data={
     //     "search_word":word
     // };
-    api.send(path,"get").then(function (data) {
+    api.send(url).then(function (data) {
         res.json(data);
     });
 });
--- routes/utils/api.js
+++ routes/utils/api.js
@@ -5,40 +5,47 @@ var Q = require('q');
 var request = require('request');
 
 var api={
-    "send" : function send(path,method){
+    "send" : function send(url){
         var deferred = Q.defer();
 
-
-        var options = {
-            hostname: 'http://192.168.203.94',
-            port: 8000,
-            path: path,
-            method: method,
-        };
-
-        var req = http.request(options, function(res) {
-            console.log('STATUS: ' + res.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
-            res.setEncoding('utf8');
-            res.on('data', function (chunk) {
-                console.log('BODY: ' + chunk);
-                deferred.resolve(data);
-            });
-        });
-
-        req.on('error', function(e) {
-            console.log('problem with request: ' + e.message);
-            deferred.reject(e);
-        });
-
-// write data to request body
-//        req.write('data\n');
-//        req.write('data\n');
-        req.end();
+        //方式一
+        // var options = {
+        //     hostname: 'http://192.168.203.94',
+        //     port: 8000,
+        //     path: path,
+        //     method: method,
+        // };
+        //
+        // var req = http.request(options, function(res) {
+        //     console.log('STATUS: ' + res.statusCode);
+        //     console.log('HEADERS: ' + JSON.stringify(res.headers));
+        //     res.setEncoding('utf8');
+        //     res.on('data', function (chunk) {
+        //         console.log('BODY: ' + chunk);
+        //         deferred.resolve(data);
+        //     });
+        // });
+        //
+        // req.on('error', function(e) {
+        //     console.log('problem with request: ' + e.message);
+        //     deferred.reject(e);
+        // });
+        //
+        // req.end();
         //deferred.resolve(str);
         //deferred.reject(e);
+        //方式二
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body) ;
+                deferred.resolve(body);
+            }else if(error){
+                deferred.reject(error);
+            }
+        });
+
         return deferred.promise;
     }
-}
+};
 
 module.exports = api;
